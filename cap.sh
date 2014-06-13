e6c96d971263f6029d8b2dfc13be62eea40c5a4d
Daryl Sew <darylsew@gmail.com>
2014-06-13 00:55:35 -0700

    added random characters instead of timestamp

--- cap.sh
+++ cap.sh
@@ -1,5 +1,8 @@
-scrot -s
-scp *.png root@107.170.117.30:/var/cafedaydream/
-filename=`ls *.png`
-echo "http://cafedaydream.com/$filename" | xclip -sel clip
-rm *.png
+#!/bin/bash
+filename=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 3 | head -n1).png
+scrot -s ~/speedcap/$filename
+scp ~/speedcap/$filename root@107.170.117.30:/var/cafedaydream/
+url="http://cafedaydream.com/$filename"
+echo $url | xclip -sel clip
+rm ~/speedcap/$filename
+chromium $url
